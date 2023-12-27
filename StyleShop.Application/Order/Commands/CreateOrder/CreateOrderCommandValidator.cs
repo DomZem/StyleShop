8611cc8e50ced56af923cf0cc81f6b9eded9dd33
Dominik <dominik.zemlik.dev@gmail.com>
2023-12-27 15:00:48 +0100

    Create validation for CreateProductCommand data

---
+++ StyleShop.Application/Order/Commands/CreateOrder/CreateOrderCommandValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using FluentValidation;
+
+namespace StyleShop.Application.Order.Commands.CreateOrder
+{
+    public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+    {
+        public CreateOrderCommandValidator()
+        {
+            RuleFor(o => o.Street)
+                .NotEmpty()
+                .MinimumLength(1)
+                .MaximumLength(50);
+            RuleFor(o => o.City)
+                .NotEmpty()
+                .MinimumLength(1)
+                .MaximumLength(50);
+
+            RuleFor(o => o.PostalCode)
+                .NotEmpty()
+                .MinimumLength(1)
+                .MaximumLength(25);
+
+            RuleFor(o => o.Country)
+                .NotEmpty()
+                .MinimumLength(3)
+                .MaximumLength(50);
+
+            RuleFor(o => o.ProductQuantity)
+                .GreaterThanOrEqualTo(1);
+        }
+    }
+}
